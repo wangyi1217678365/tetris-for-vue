305ad4f68e4ad242af487f772c33f033ed3256b8
Wangbotao <1217678365@qq.com>
2022-05-15 19:57:56 +0800

    feat: 配置根路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import pxtovw from 'postcss-px-to-viewport'
+import * as path from 'path'
 const loder_pxtovw = pxtovw({
   viewportWidth: 750,      // 视窗的宽度，对应的是我们设计稿的宽度，一般是750
   viewportHeight: 1334,    // 视窗的高度，根据750设备的宽度来指定，一般指定1334，也可以不配置
@@ -20,5 +21,13 @@ export default defineConfig({
         loder_pxtovw
       ]
     }
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve('./src')
+      }
+    ]
   }
 })
